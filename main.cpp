5106cc5d65ac37856af07f9a3013b39ec8b52ba2
Olyaaaaaaa <163026273+Olyaaaaaaa@users.noreply.github.com>
2024-03-19 23:34:56 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,116 +1,72 @@
 #include <iostream>
-#include <exception>
-#include "queue.h"
+#include "bits.h"
 using namespace std;
 int main()
 {
-	Queue MyQueue;
-	Queue OtherQueue;
-	OtherQueue.push(21);
-	OtherQueue.push(14);
-	OtherQueue.push(98);
-	OtherQueue.push(27);
-	OtherQueue.push(22);
-	OtherQueue.push(91);
-	MyQueue.push(8);
-	MyQueue.push(9);
-	MyQueue.push(10);
-	MyQueue.push(2);
-	MyQueue.push(45);
-	MyQueue.push(33);
-	MyQueue.push(71);
-	cout << "My queue: "; MyQueue.print();
-	cout << "Other queue: "; OtherQueue.print();
-	cout << "Checking emptyness..." << endl;
-	cout << "Is my queue empty(0 - no, 1 - yes): " << MyQueue.isEmpty() << endl;
-	cout << "Queues after swaping: " << endl;
-	MyQueue.swap(OtherQueue);
-	cout << "My queue: "; MyQueue.print();
-	cout << "Other queue: "; OtherQueue.print();
+	Bitset bits;
+	cout << "Our bitset now: ";
+	bits.print_bitset();
+	cout << "Trying to set bits: " << endl;
 	try
 	{
-		cout <<"Back element: "<< MyQueue.back() << endl;
+		bits.set_bit(2);
+		bits.set_bit(4);
+		bits.set_bit(5);
+		bits.set_bit(6);
+		bits.set_bit(-678);
 	}
 	catch (exception& e)
 	{
 		cout << e.what() << endl;
 	}
+	cout << "Bitset after seting bits: ";
+	bits.print_bitset();
 	try
 	{
-		cout <<"Front element: "<< MyQueue.front() << endl;
+		bits.reset_bit(5);
+		bits.reset_bit(6);
+		bits.reset_bit(-89);
 	}
 	catch (exception& e)
 	{
 		cout << e.what() << endl;
 	}
-	MyQueue.pop();
-	MyQueue.pop();
-	MyQueue.pop();
-	cout << "My queue after poping: "; MyQueue.print();
-	cout << "Other queue: "; OtherQueue.print();
-	MyQueue = OtherQueue;
-	cout << "Work of operator=, My queue: ";
-	MyQueue.print();
-	cout << "Size of my queue: " << MyQueue.size() << endl;
-	cout << endl;
-	cout << endl;
-
-
-	cout << "Working with priority queue." << endl;
-	PriorityQueue Q;
-	PriorityQueue P;
-	P.enqueue(40, 6); 
-	P.enqueue(34, 2); 
-	Q.enqueue(11, 5); 
-	Q.enqueue(21, 4);
-	Q.enqueue(4, 1); 
-	Q.enqueue(5, 1); 
-	Q.enqueue(41, 4); 
-	Q.enqueue(7, 2);
-	Q.enqueue(8, 3); 
-	Q.enqueue(5, 5); 
-	Q.enqueue(15, 5);
-	Q.enqueue(67, 1);
-	cout << "Our queue: "; Q.print();
-	cout << "Size of our queue: " << Q.size() << endl;
-	cout << "Other queue: "; P.print();
-	cout << "Checking emptyness..." << endl;
-	cout << "Is our queue empty(0 - no, 1 - yes): " << Q.isEmpty() << endl;
-	Q.swap(P);
-	cout << "Queues after swaping: " << endl;
-	cout << "My queue: "; Q.print();
-	cout << "Other queue: ";P.print();
+	cout << "Bitset afyer reseting bits: "; bits.print_bitset();
 	try
 	{
-		Q.dequeue();
-		Q.dequeue();
-		Q.dequeue();
+		bits.flip_bit(11);
+		bits.flip_bit(12);
+		bits.flip_bit(111);
 	}
 	catch (exception& e)
 	{
 		cout << e.what() << endl;
 	}
+	cout << "Bitset after fliping bits: "; bits.print_bitset();
 	try
 	{
-		cout << "Our queue after dequeueing: "; Q.print();
+		cout << "Index 12: " << bits[12] << endl;
+		cout << "Index 11: " << bits[11] << endl;
+		cout << bits[77] << endl;
 	}
 	catch (exception& e)
 	{
 		cout << e.what() << endl;
 	}
-	try
-	{
-		cout << "Front element: " << Q.front() << endl;
-		cout << "Back element: " << Q.Back() << endl;
-	}
-	catch (exception& e)
-	{
-		cout << e.what() << endl;
-	}
-	cout << "Deleting queues: " << endl;
-	Q.clear();
-	P.clear();
-	MyQueue.makeEmpty();
-	OtherQueue.makeEmpty();
+	cout << "Check if all elements set(0 - not, 1 - yes): " << bits.all_bits() << endl;
+	cout << "Check if any element set(0 - not, 1 - yes): " << bits.any_bit() << endl;
+	cout << "Check if none element set(0 - not, 1 - yes): " << bits.none_bit() << endl;
+	cout << "Count: " << bits.count() << endl;
+	cout << "Testing 1 bit: " << bits.test_bit(1) << endl;
+	cout << "Testing 2 bit: " << bits.test_bit(2) << endl;
+	cout << "Testing 7 bit: " << bits.test_bit(7) << endl;
+	Bitset testing;
+	testing.set_bit(31);
+	bits |= testing;
+	cout << "Using |= : "; bits.print_bitset();
+	~bits;
+	cout << "Using ~ : "; bits.print_bitset();
+	bits &= testing;
+	cout << "Using &= : "; bits.print_bitset();
 	return 0;
 }
